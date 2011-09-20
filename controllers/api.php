2b697a964727a2af8d2e71286ab4542e9cd8bbf4
Brennan Novak <brennan@reverseproductions.com>
2011-09-19 14:47:32 -1000

    gutted most shiz, made API routes and basic model should be working

---
+++ controllers/api.php
@@ -0,0 +1,90 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * Emoome API : Module : Social-Igniter
+ *
+ */
+class Api extends Oauth_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('emoome_model');
+	}
+
+	// Log Feeling
+	function log_feeling_authd_post()
+	{
+		// Add Log	
+		if ($log_id = $this->emoome_model->add_log($this->oauth_user_id, 'feeling'))
+		{
+			// Add Word
+			$feeling = $this->emoome_model->add_word_link($log_id, $this->input->post('feeling'));
+			
+			// Add Action
+			$action_id = $this->emoome_model->add_action($log_id, $this->input->post('action'));
+
+			// Add Descriptors			
+//			$describe_1 = $this->emoome_model->add_word_link($log_id, $this->input->post('describe_1'));
+//			$describe_2 = $this->emoome_model->add_word_link($log_id, $this->input->post('describe_2'));
+//			$describe_3 = $this->emoome_model->add_word_link($log_id, $this->input->post('describe_3'));
+		
+            $message = array('status' => 'success', 'message' => 'Success logged feeling', 'data' => $log_id.' '.$action_id.' '.$feeling);		
+		}
+		else
+		{
+            $message = array('status' => 'error', 'message' => 'Could not save log');
+		}
+
+        $this->response($message, 200);
+	}
+
+	// Log Music
+	function log_music_authd_post()
+	{
+
+		if ($_POST)
+		{
+            $message = array('status' => 'success', 'message' => 'Success item added to cart', 'data' => $_POST);		
+		}
+		else
+		{
+            $message = array('status' => 'error', 'message' => 'Could not find any classes');
+		}
+
+        $this->response($message, 200);	
+	}	
+
+	// Log Photo
+	function log_image_authd_post()
+	{
+
+		if ($_POST)
+		{
+            $message = array('status' => 'success', 'message' => 'Success item added to cart', 'data' => $_POST);		
+		}
+		else
+		{
+            $message = array('status' => 'error', 'message' => 'Could not find any classes');
+		}
+
+        $this->response($message, 200);	
+	}		
+
+	// Log Place
+	function log_place_authd_post()
+	{
+
+		if ($_POST)
+		{
+            $message = array('status' => 'success', 'message' => 'Success item added to cart', 'data' => $_POST);		
+		}
+		else
+		{
+            $message = array('status' => 'error', 'message' => 'Could not find any classes');
+		}
+
+        $this->response($message, 200);	
+	}	
+	
+}
--- models/emoome_model.php
+++ models/emoome_model.php
@@ -1,37 +1,130 @@
 <?php
-
-class Module_template_model extends CI_Model
+//Contribute deals with making data contributions
+class Emoome_model extends CI_Model 
 {
-    
     function __construct()
     {
         parent::__construct();
+
+        $this->load->library('natural_language');        
     }
-    
-    function get_content($limit=8)
-    {
- 		$this->db->select('table.status_id, table.user_id');
- 		$this->db->from('table');    
- 		$this->db->join('users', 'users.user_id = table.user_id'); 				
- 		$this->db->order_by('created_at', 'desc'); 
-		$this->db->limit($limit);    
- 		$result = $this->db->get();	
+
+	// Log 
+	function get_log_user($user_id)
+	{
+		return FALSE;
+	}
+	
+	function add_log($user_id, $type)
+	{
+		$log_data = array(
+			'user_id'		=> $user_id,
+			'type'			=> $type,
+			'created_at'	=> unix_to_mysql(now())
+		);
+
+		$this->db->insert('emoome_log', $log_data);
+
+		if ($log_id = $this->db->insert_id())
+		{
+			return $log_id;
+    	}
+
+	    return FALSE;
+	}
+
+
+	// Actions
+	function get_action($action_id)
+	{
+		$this->db->select('*');
+		$this->db->from('emoome_actions');
+		$this->db->where('action_id', $action_id);
+		$this->db->limit(1);
+ 		$result = $this->db->get();
  		return $result->result();	      
-    }
-    
-    function add_content($user_id, $status_data)
+	}
+
+	function add_action($log_id, $action) 
+	{	
+		$action_data = array(
+			'log_id' 		 => $log_id,
+			'action' 	 	 => $action
+		);
+		
+		$this->db->insert('emoome_actions', $action_data);
+
+		if ($action_id = $this->db->insert_id())
+		{	
+			return $action_id;
+    	}
+	
+		return FALSE;
+	}
+
+
+	// Words & Word Link
+	function check_word($word)
+	{
+		$this->db->select('*');
+		$this->db->from('emoome_words');
+		$this->db->where('word', $word);
+		$this->db->limit(1);
+ 		
+ 		if ($result = $this->db->get()->row())	
+ 		{
+ 			return $result;
+ 		}
+
+		return FALSE;
+	}
+
+
+    function add_word($word)
     {
- 		$data = array(
-			'user_id' 	 			=> $user_id,
-			'source'				=> $status_data['source'],
-			'text'  	 			=> $status_data['text'],
-			'lat'		 			=> $status_data['lat'],
-			'long'					=> $status_data['long'],
-			'created_at' 			=> unix_to_mysql(now())
-		);	
-		$insert 	= $this->db->insert('table', $data);
-		$status_id 	= $this->db->insert_id();
-		return $this->db->get_where('table', array('item_id' => $status_id))->row();	
-    }    
+    	$stem = $this->natural_language->stem($word);
     
+ 		$word_data = array(
+			'word' 	 	=> $word,
+			'stem'		=> $stem,
+			'type'		=> ''
+		);	
+
+		$this->db->insert('emoome_words', $word_data);
+
+		if ($word_id = $this->db->insert_id())
+		{	
+			return $word_id;
+    	}
+    
+	    return FALSE;
+    }	
+	
+	// Log Link
+	function add_word_link($log_id, $word)
+	{	
+		$check_word = $this->check_word(strtolower($word));
+	
+		if ($check_word)
+		{
+			$word_id = $check_word->word_id;		
+		}
+		else
+		{
+			$word_id = $this->add_word(strtolower($word));
+		}
+		
+		$link_data = array(
+			'log_id'	=> $log_id,
+			'word_id'	=> $word_id
+		);			
+
+		$this->db->insert('emoome_words_link', $link_data);	
+		
+		$word_link_id = $this->db->insert_id();
+	
+		return $word.' '.$word_id.' '.$word_link_id;
+		
+	}
+
 }
