071a7a68826101199bab444d9028bb44f0d56d4d
Brennan Novak <brennan@reverseproductions.com>
2011-10-07 14:03:29 -0700

    added database schema

--- install/DATABASE.sql
+++ install/DATABASE.sql
@@ -1,1 +1,31 @@
-INSERT INTO `settings` VALUES(NULL, 1, 'module-template', 'enabled', 'TRUE');
+INSERT INTO `settings` VALUES(NULL, 1, 'emoome', 'enabled', 'TRUE');
+
+CREATE TABLE `emoome_words_link` (
+  `link_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `log_id` int(11) DEFAULT NULL,
+  `word_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`link_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `emoome_words` (
+  `word_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `word` varchar(128) DEFAULT NULL,
+  `stem` varchar(128) DEFAULT NULL,
+  `type` char(1) DEFAULT NULL,
+  PRIMARY KEY (`word_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `emoome_log` (
+  `log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `type` char(16) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `emoome_actions` (
+  `action_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `log_id` int(11) DEFAULT NULL,
+  `action` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`action_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
