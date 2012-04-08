a9cb48940ca95d2abe59021b60f87d17ffd4b3ae
Brennan Novak <brennan@reverseproductions.com>
2012-04-08 01:50:08 -0700

    fixed styles and made basic logging of feeling work

--- views/contribute/feeling.php
+++ views/contribute/feeling.php
@@ -1,32 +1,54 @@
 <!-- Log Type: Feeling -->
-<div id="log_feeling" class="view_center">
+<div id="log_feeling" class="content_center text_center">
 	<h1>How do you feel right now?</h1>	
 	<p><input type="text" name="log_feeling" id="log_val_feeling" value=""></p>		
-	<p><a id="log_feel_next" class="form_button center" href="javascript:logFeeling()">Next</a></p>
+	<p><a id="log_feel_next" class="button" href="javascript:logFeeling()">Next</a></p>
 </div>
 
-<div id="log_action" class="view_center">
+<div id="log_action" class="content_center text_center">
 	<h1>What is one thing you did today?</h1>
 	<!-- <p><input type="text" name="log_action" id="log_val_action" value=""></p> -->
 	<p><textarea name="log_action" id="log_val_action"></textarea></p>
-	<p><a id="log_action_next" href="javascript:logAction()" class="form_button center">Next</a></p>
+	<p><a id="log_action_next" href="javascript:logAction()" class="button">Next</a></p>
 </div>
 
-<div id="log_describe" class="view_center">
+<div id="log_describe" class="content_center text_center">
 	<h1>Describe in three words</h1>
 	<p id="log_describe_this"></p>
 	<p><input type="text" name="log_describe_1" id="log_val_describe_1" value=""></p>
 	<p><input type="text" name="log_describe_2" id="log_val_describe_2" value=""></p>
 	<p><input type="text" name="log_describe_3" id="log_val_describe_3" value=""></p>
-	<p><a id="log_describe_next" class="form_button center" href="javascript:logDescribe();">Finish</a></p>
+	<p><a id="log_describe_next" class="button" href="javascript:logDescribe();">Finish</a></p>
 </div>
 
 <form name="log_data" id="log_data">
-	<input type="hidde" name="log_type" value="feeling">
+	<input type="hidden" name="log_type" value="feeling">
 </form>
 
 <!-- Log Complete Screen -->
 <div id="log_thanks" class="view_center">
 	<h1>Thanks :)</h1>
-	<p><a id="log_action_next" class="form_button center" href="javascript:tabLog()">Another</a></p>
+	<p><a id="log_action_next" class="button" href="#">Another</a></p>
 </div>
+
+<script type="text/javascript">
+$(document).ready(function()
+{
+	$('#log_feeling, #log_action, #log_describe, #log_thanks').hide();
+	$('#log_feeling').delay(500).fadeIn();	
+
+	// Hijack Spacebar in a few places...
+/*
+	$('#log_val_feeling').jkey('space',function()
+	{
+		alert('Please enter only single word feeling (no spaces)');
+	});
+
+	$('#log_val_describe_1, #log_val_describe_2, #log_val_describe_3').jkey('space',function()
+	{
+		alert('Please enter only single words to describe your action (no spaces)');
+	});
+*/
+
+});
+</script>
--- views/contribute/index.php
+++ views/contribute/index.php
@@ -1,5 +1,43 @@
-<h1>Contribute</h1>
+<div id="content_test" class="content_center text_center">
+	
+	<h1>Contribute</h1>
+		
+	<p>
+	<a id="button_cat_feeling" class="category_button" href="<?= base_url() ?>emoome/contribute/feeling">
+	  How Do You Feel Right Now?
+	</a>
+	</p>
+	
+	<p>
+	<a id="button_cat_memory" class="category_button" href="<?= base_url() ?>emoome/contribute/memory">
+	  Memory Mapper
+	</a>
+	</p>
+	
+	<div id="blah_blah_blah"></div>
 
-<a class="category_button" href="<?= base_url() ?>emoome/contribute/feeling">How Do You Feel Right Now?</a>
+</div>
 
-<a class="category_button" href="<?= base_url() ?>emoome/contribute/memory">Memory Mapper</a>
+
+<script type="text/javascript" src="<?= $site_assets ?>js/raphael.js" charset="utf-8"></script>
+<script type="text/javascript" src="<?= $site_assets ?>js/raphael-svg-import.js" charset="utf-8"></script>
+<script type="text/javascript" src="<?= $site_assets ?>js/raphael-shapes.js" charset="utf-8"></script>
+
+<script type="text/javascript">
+$(document).ready(function()
+{
+	//$('#button_cat_feeling').svg({loadURL: 'http://localhost/application/modules/emoome/assets/icons/profile_1899.svg'});
+	//$('#button_cat_memory').svg({loadURL: 'http://localhost/application/modules/emoome/assets/icons/brain_685.svg'});
+
+	//var profile = new Raphael(document.getElementById('button_cat_feeling'), 100, 100).path(emoome_icons.profile).attr({fill: '#000000'});
+	//var brain = new Raphael(document.getElementById('button_cat_memory'), 100, 100).path(emoome_icons.brain).attr({fill: '#000000'});
+	
+//	var c = paper.image("http://localhost/application/modules/emoome/assets/icons/brain_685.svg", 10, 10, 80, 80);
+
+	var feelings = Raphael(document.getElementById('button_cat_feeling'), 100, 100).importSVG(emoome_icons.profile);
+	var brain = Raphael(document.getElementById('button_cat_memory'), 100, 100).importSVG(emoome_icons.brain);
+
+
+
+});
+</script>
