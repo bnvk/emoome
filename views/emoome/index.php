f9ea381800299dae054e75e672991806e31adc0e
Brennan Novak <brennan@reverseproductions.com>
2012-04-10 11:58:00 -0700

    made homepage cool

--- views/emoome/index.php
+++ views/emoome/index.php
@@ -1,4 +1,15 @@
-<div id="index_holder">
-	<img src="<?= $site_assets ?>images/title.png">
-	<p>iPhone & Mobile Web apps coming soon. Wanna get in on the beta? <a href="mailto:info@emoo.me">Contact Us</a></p>
+<div id="content_test" class="content_center text_left">
+
+	<div id="index_define">
+		<h1 id="index_name"><span>emoome</span></h1>		
+		<p class="index_define"><strong>emo</strong> - <em>a complex psychophysiological experience of an individual's state of mind as interacting with biochemical (internal) and environmental (external) influences</em></p> 
+		<p class="index_define"><strong>ome</strong> - <em>as used in biology, refers to a totality of some sort</em></p>
+	</div>
+
+	<h3>~ Quantify Yourself</h3>
+	<h3>~ Emotional Journaling</h3>
+	<h3>~ Memory Mapping</h2>
+	<h3>~ Linguistics + Psychology</h3>
+	<h3>~ <a href="mailto:info@emoo.me">Contact</a></h3>
+
 </div>
