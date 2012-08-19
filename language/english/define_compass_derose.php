fbbdee0b8b7835a22a088ae3ccceff98866ff9ee
Brennan Novak <hi@brennannovak.com>
2012-08-19 15:07:53 -0700

    refined define_compass_derose lang file

--- language/english/define_compass_derose.php
+++ language/english/define_compass_derose.php
@@ -1,104 +1,62 @@
 <?php if (!defined("BASEPATH")) exit("No direct script access allowed");
 
 // Pleasure 
-$lang["happy"] = array("airy, amused, animated, beatific, blissful, blithe, bright, brisk, buoyant, cheerful, cheery, comfortable, contented, convivial, debonair, ecstatic, elated, enthusiastic, excited, exhilarated, exultant, festive, free & easy, frisky, genial, glad, gleeful, great, happy, high-spirited, hilarious, humorous, important, inspired, jaunty, jocular, jolly, jovial, joyful, joyous, jubilant, laughting, lighthearted, lively, lucky, merry, mirthful, overjoyed, peaceful, playful, pleased, proud, rapturous, satisfied, saucy, self-satisfied, serene, sparkling, spirited, sprightly, sunny, terrific, thankful, tranquil, transported, vivacious");
-$lang["ecstatic"] = array("delighted", "ecstatic", "fabulous", "fantastic", "overjoyed");
+$lang["happy"]			= array("airy", "amused", "animated", "beatific", "blissful", "blithe", "bright", "brisk", "buoyant", "cheerful", "cheery", "comfortable", "contented", "convivial", "debonair", "ecstatic", "elated", "enthusiastic", "excited", "exhilarated", "exultant", "festive", "free", "frisky", "genial", "glad", "gleeful", "great", "happy", "high-spirited", "hilarious", "humorous", "important", "inspired", "jaunty", "jocular", "jolly", "jovial", "joyful", "joyous", "jubilant", "laughting", "lighthearted", "lively", "lucky", "merry", "mirthful", "overjoyed", "peaceful", "playful", "pleased", "proud", "rapturous", "satisfied", "saucy", "self-satisfied", "serene", "sparkling", "spirited", "sprightly", "sunny", "terrific", "thankful", "tranquil", "transported", "vivacious");
+$lang["ecstatic"]		= array("delighted", "ecstatic", "fabulous", "fantastic", "overjoyed");
 
 // Pain
-$lang["angry"] = array("acrimonious", "angry", "annoyed", "appalled", "bitter", "boiling", "cross", "devastated", "disgusted", "enraged", "frustrated", "fuming", "furious", "hostile", 
-"in a huff",
-"in a stew", 
-"incensed", "indignant", "inflamed", "infuriate", "irate", "irritated", "livid", "mad", "offended", "outraged", "piqued", "provoked", "rageful", "resentful", "sullen", 
-"up in arms", 
-"virulent", 
-"worked up", 
-"wrathful", 
-"wrought up");
-
-$lang["sad"] = array("aching", "afflicted", "agonized", "anguished", "bereaved", "blue", "cheerless", "clouded", "crestfallen", "crushed", "dark", "dejected", "depressed", "despairing", "despondent", "disconsolate", "discontented", "discouraged", "disheartened", "dismal", "displeased", "distressed", "dolorous", "down", "downcast", "downhearted", "dreadful", "dreary", "dull", "embarrassed", "flat", "frowning", "funereal", "gloomy", "glum", "grief", "grieved", "guilt", "hapless", "heartbroken", "heavyhearted", "humiliated", "hurt", 
-"ill at ease", 
-"in despair", "in pain", "in the dumps", "injured", "joyless", "lonely", "low spirited", "low", "lugubrious", "melancholy", "moody", "moping", "mournful", 
-"mumpish", "offended", "oppressed", 
-"out of sorts", 
-"pathetic", "piteous", "regretful", "remorse", "rueful", "sad", "shamed", "shocked", "somber", "sorrowful", "spiritless", "suffering", "sulky", "sullen", "tortured", "tragic", "unhappy", "victimized", "woebegone", "woeful", "worried");
+$lang["angry"]			= array("acrimonious", "angry", "annoyed", "appalled", "bitter", "boiling", "cross", "devastated", "disgusted", "enraged", "frustrated", "fuming", "furious", "hostile", "huff", "stew", "incensed", "indignant", "inflamed", "infuriate", "irate", "irritated", "livid", "mad", "offended", "outraged", "piqued", "provoked", "rageful", "resentful", "sullen", "arms", "virulent", "worked", "wrathful", "wrought");
+$lang["sad"]			= array("aching", "afflicted", "agonized", "anguished", "bereaved", "blue", "cheerless", "clouded", "crestfallen", "crushed", "dark", "dejected", "depressed", "despairing", "despondent", "disconsolate", "discontented", "discouraged", "disheartened", "dismal", "displeased", "distressed", "dolorous", "down", "downcast", "downhearted", "dreadful", "dreary", "dull", "embarrassed", "flat", "frowning", "funereal", "gloomy", "glum", "grief", "grieved", "guilt", "hapless", "heartbroken", "heavyhearted", "humiliated", "hurt", "ill", "despair", "pain", "dumps", "injured", "joyless", "lonely", "low spirited", "low", "lugubrious", "melancholy", "moody", "moping", "mournful", "mumpish", "offended", "oppressed", "pathetic", "piteous", "regretful", "remorse", "rueful", "sad", "shamed", "shocked", "somber", "sorrowful", "spiritless", "suffering", "sulky", "sullen", "tortured", "tragic", "unhappy", "victimized", "woebegone", "woeful", "worried");
 
 // Power/Control/Responsibility
-$lang["irresistible"] = array("aggressive", "exhuberent", "immortal", "indestructible", "invincible", "invulnerable", "irresistible", "powerful", "unstoppable")
-$lang["powerless"] = array("bashful", 
-"beaten down", "blocked", "defeated", "discouraged", "disorganized", 
-"exhausted hopeless", "irresponsible", "overwhelmed", "powerless", "stifled", "thwarted", 
-"worn down", "worn out");
-$lang["out_of_control"] = array("abused", "careless", "controlled", "forced", "impotent", "obligated", "obliterated",
-"out of control", "powerless", "reckless", "vulnerable", "weak");
-$lang["apathetic"] = array("apathetic, complacent, full of ennui, immobilized, lazy, lethargic, numb, passive, quiescent, unconcerned, unmotivated");
-$lang["adequate"] = array("adequate", "capable", "competent", "composed", "confident", "encouraged", "excited", 
-"in control", "organized", "responsible");
+$lang["irresistible"]	= array("aggressive", "exhuberent", "immortal", "indestructible", "invincible", "invulnerable", "irresistible", "powerful", "unstoppable")
+$lang["powerless"]		= array("bashful", "beaten", "blocked", "defeated", "discouraged", "disorganized", "exhausted", "hopeless", "irresponsible", "overwhelmed", "powerless", "stifled", "thwarted", "worn");
+$lang["out_of_control"]	= array("abused", "careless", "controlled", "forced", "impotent", "obligated", "obliterated", "powerless", "reckless", "vulnerable", "weak");
+$lang["apathetic"]		= array("apathetic, complacent, full of ennui, immobilized, lazy, lethargic, numb, passive, quiescent, unconcerned, unmotivated");
+$lang["adequate"]		= array("adequate", "capable", "competent", "composed", "confident", "encouraged", "excited", "control", "organized", "responsible");
 
 // Attachment
-$lang["alone"] = array("alone", 
-"cut off", "excluded", "forsaken", "isolated",
-"left out", "rejected", 
-"shut out", "detached", "lonely", "lonesome", "misunderstood");
-$lang["independent"] = array("arrogant", "autonomous", "cocky", "independent", "strong", "macho");
-$lang["attached"] = array("affectionate, attached, belonging, captivated, cherished, compassionate, connected, empathetic, included, liked, loved, loving, understood");
-$lang["codependent"] = array("addicted", "codependent", "insecure", "needy", "sympathetic");
-$lang["hated"] = array("abandoned, chastised, criticized, deserted, discarded, forsaken, hated, ignored, left out, let down, overlooked, punished, rejected, replaced, unapproved of, unlovable, unloved");
-
-$lang["loved"] = array("accepted", "adored", "adorable", 
-"approved of", 
-"desirable", "entrusted", "loved", "validated", "valued", "welcomed");
+$lang["alone"]			= array("alone", "cut", "excluded", "forsaken", "isolated", "left", "rejected", "shut", "detached", "lonely", "lonesome", "misunderstood");
+$lang["independent"]	= array("arrogant", "autonomous", "cocky", "independent", "strong", "macho");
+$lang["attached"]		= array("affectionate, attached, belonging, captivated, cherished, compassionate, connected, empathetic, included, liked, loved, loving, understood");
+$lang["codependent"]	= array("addicted", "codependent", "insecure", "needy", "sympathetic");
+$lang["hated"]			= array("abandoned", "chastised", "criticized", "deserted", "discarded", "forsaken", "hated", "ignored", "left out", "let down", "overlooked", "punished", "rejected", "replaced", "unapproved of", "unlovable", "unloved");
+$lang["loved"] 			= array("accepted", "adored", "adorable", "approved", "desirable", "entrusted", "loved", "validated", "valued", "welcomed");
 
 // Social Standing / Face
-$lang["belittled"] = array("belittled", "chagrined", "degraded", "diminished", "discounted", "discredited", "disgraced", "disrespected", "insignificant", "underestimated", "unsupported");
+$lang["belittled"]		= array("belittled", "chagrined", "degraded", "diminished", "discounted", "discredited", "disgraced", "disrespected", "insignificant", "underestimated", "unsupported");
 
 // Embarrassed
-$lang["embarrased"] = array("ashamed", "awkward", "disparaged", "embarrassed", "guilty");
-$lang["average"] = array("average, common, ordinary");
-$lang["esteemed"] = array("admired", "appreciated", "esteemed", "respected", "revered", "significant", "supported", "valued", "worshiped");
+$lang["embarrased"]		= array("ashamed", "awkward", "disparaged", "embarrassed", "guilty");
+$lang["average"]		= array("average, common, ordinary");
+$lang["esteemed"]		= array("admired", "appreciated", "esteemed", "respected", "revered", "significant", "supported", "valued", "worshiped");
 
 // Justice
-$lang["cheated"] = array("abused", "accused", "betrayed", "cheated", "coerced", "conned", "deceived", "disparaged", "duped", "exploited", "framed", "lied to", "manipulated", "tricked", "victimized", "violated");
-$lang["singled_out"] = array("affronted", "categorized", "cross-examined", "guilty", "interrogated", "judged", "labeled", "rated", 
-"singled out", 
-"stereotyped", "wronged");
-$lang["justified"] = array("absolved", "acquitted", "appeased", "justified", "redeemed", "satisfied", "vindicated");
-$lang["entitled"] = array("entitled", "exempt", "favored", "immune", "privileged");
+$lang["cheated"]		= array("abused", "accused", "betrayed", "cheated", "coerced", "conned", "deceived", "disparaged", "duped", "exploited", "framed", "lied to", "manipulated", "tricked", "victimized", "violated");
+$lang["singled_out"]	= array("affronted", "categorized", "cross-examined", "guilty", "interrogated", "judged", "labeled", "rated", "singled", "stereotyped", "wronged");
+$lang["justified"]		= array("absolved", "acquitted", "appeased", "justified", "redeemed", "satisfied", "vindicated");
+$lang["entitled"]		= array("entitled", "exempt", "favored", "immune", "privileged");
 
 // Freedom
-$lang["trapped"] = array("deceived", "cornered", "imprisoned", "optionless", "outmaneuvered", 
-"set up", 
-"stifled", "trapped", "tricked");
-
-$lang["burdened"] = array("burdened", "obligated", "pressured", "put", "upon", "thwarted");
-$lang["free"] = array("autonomous", "free", "independent", "released", "unshackled");
+$lang["trapped"] 		= array("deceived", "cornered", "imprisoned", "optionless", "outmaneuvered", "setup", "stifled", "trapped", "tricked");
+$lang["burdened"]		= array("burdened", "obligated", "pressured", "put", "upon", "thwarted");
+$lang["free"]			= array("autonomous", "free", "independent", "released", "unshackled");
 
 // Direction/Focus
-$lang["derailed"] = array("derailed", "disjointed", "disoriented", "torn");
-$lang["lost"] = array("baffled", "bewildered", "confused", "lost", "unfocussed");
-$lang["focused"] = array("committed", "complacent", "determined", "focused", "in the zone");
-$lang["obsessed"] = array("compelled", "consumed", "obsessed");
+$lang["derailed"]		= array("derailed", "disjointed", "disoriented", "torn");
+$lang["lost"]			= array("baffled", "bewildered", "confused", "lost", "unfocussed");
+$lang["focused"]		= array("committed", "complacent", "determined", "focused", "in the zone");
+$lang["obsessed"]		= array("compelled", "consumed", "obsessed");
 
 // Desire/Interest
-$lang["demoralized"] = array("demoralized", "disappointed", "discouraged", "disheartened", "disillusioned", "disinclined", "repulsed", "stifled", "thwarted");
-$lang["bored"] = array("ambivalent", "apathetic", "bored", "complacent", 
-"full of ennui", 
-"indifferent", "lackadaisical", "unmotivated");
-
-$lang["attracted"] = array("absorbed", "affected", "agog", 
-"full of anticipation", 
-"anxious", "attracted", "avid", "challenged", "concerned", "confident", "craving", "curious", "dedicated", "desirous", "eager", "earnest", "enchanted", "engrossed", "enthusiastic", "excited", "fascinated", "fervent", "fervid", "hopeful", "inquisitive", "inspiring", "intent", "interested", "intrigued", "keen", "motivated", "needed", "nosey", "snoopy", "zealous");
-$lang["lustful"] = array("addicted", "ardent", "aroused", "horny", 
-"hot and bothered", 
-"infatuated", "lustful", "needy", "passionate", 
-"turned on", 
-"yearning");
+$lang["demoralized"] 	= array("demoralized", "disappointed", "discouraged", "disheartened", "disillusioned", "disinclined", "repulsed", "stifled", "thwarted");
+$lang["bored"] 			= array("ambivalent", "apathetic", "bored", "complacent", "ennui", "indifferent", "lackadaisical", "unmotivated");
+$lang["attracted"] 		= array("absorbed", "affected", "agog", "anticipation", "anxious", "attracted", "avid", "challenged", "concerned", "confident", "craving", "curious", "dedicated", "desirous", "eager", "earnest", "enchanted", "engrossed", "enthusiastic", "excited", "fascinated", "fervent", "fervid", "hopeful", "inquisitive", "inspiring", "intent", "interested", "intrigued", "keen", "motivated", "needed", "nosey", "snoopy", "zealous");
+$lang["lustful"] 		= array("addicted", "ardent", "aroused", "horny", "infatuated", "lustful", "needy", "passionate", "sexual", "sexy", "yearning");
 
 // Safety/Security
-$lang["fearful"] = array("afraid, aghast, alarmed, anxious, appalled, apprehensive, attacked, awed, cautious, chicken, cowardly, defenseless, diffident, dismayed, doubtful, exposed, fainthearted, fearful, fidgety, frightened, hesitant, horrified, hysterical, in fear, insecure, irresolute, menaced, misgiving, nervous, panicked, petrified, phobic, quaking, restful, scared, shaky, shocked, suspicious, terrified, terrorized, threatened, timid, timorous, trembling, tremulous, upset, worried, yellow");
-$lang["anxious"] = array("apprehensive", "cautious", "concerned", "distrustful", "doubtful", "dubious", 
-"full of misgiving", 
-"hesitant", "indecisive", "pensive", "perplexed", "questioning", "skeptical", "suspicious", "tense", "unbelieving", "uncertain", "uncomfortable", "wavering");
-$lang["fearless"] = array("audacious", "bold", "brave", "certain", "confident", "courageous", "daring", "dauntless", "determined", "encouraged", "enterprising", "gallant", "hardy", "heroic", "reassured", "resolute", "secure", "self-reliant", "spirited", "stout", "hearted");
-
-$lang["safe"] = array("at ease", "calm", "comfortable", "composed", "peaceful", "secure");
-$lang["surprise"] = array("astonished", "bewildered", "confused", "shocked", "startled", "surprised");
+$lang["fearful"]		= array("afraid, aghast, alarmed, anxious, appalled, apprehensive, attacked, awed, cautious, chicken, cowardly, defenseless, diffident, dismayed, doubtful, exposed, fainthearted, fearful, fidgety, frightened, hesitant, horrified, hysterical, in fear, insecure, irresolute, menaced, misgiving, nervous, panicked, petrified, phobic, quaking, restful, scared, shaky, shocked, suspicious, terrified, terrorized, threatened, timid, timorous, trembling, tremulous, upset, worried, yellow");
+$lang["anxious"]		= array("apprehensive", "cautious", "concerned", "distrustful", "doubtful", "dubious", "misgiving", "hesitant", "indecisive", "pensive", "perplexed", "questioning", "skeptical", "suspicious", "tense", "unbelieving", "uncertain", "uncomfortable", "wavering");
+$lang["fearless"]		= array("audacious", "bold", "brave", "certain", "confident", "courageous", "daring", "dauntless", "determined", "encouraged", "enterprising", "gallant", "hardy", "heroic", "reassured", "resolute", "secure", "self-reliant", "spirited", "stout", "hearted");
+$lang["safe"]			= array("at ease", "calm", "comfortable", "composed", "peaceful", "secure");
+$lang["surprise"]		= array("astonished", "bewildered", "confused", "shocked", "startled", "surprised");
