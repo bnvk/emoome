8fc22b6296266e9c12f36bfbad64e376a901acd9
Brennan Novak <hi@brennannovak.com>
2012-08-24 12:19:03 -0700

    fixing more date usuability issues with search Ref #6

--- views/visualize/search.php
+++ views/visualize/search.php
@@ -1,12 +1,28 @@
 <h2 id="search_title">How Do I Feel Between
 	<!-- Between (hours), Between (dates), About (enter keyword), At (location) -->
-	<?= form_dropdown('start_time', config_item('time_increments_hours_twelve'), date('h'), 'id="start_time"') ?>
-	<?= form_dropdown('start_meridian', config_item('time_meridian'), date('A'), 'id="start_meridian"') ?>
-	 to 
-	<?php 
+	<?php
+	$start_hour = date('h');
 	$end_hour = date('h') + 3;
-	$end_meridian = date('A');
-	
+
+	if ($start_hour > 12)
+	{
+		$start_hour = $start_hour - 12;
+	}
+
+	if ($end_hour > 12)
+	{
+		$end_hour = $end_hour - 12;
+		$end_meridian = 'PM';
+	}
+	else
+	{
+		$end_hour = $end_hour;
+		$end_meridian = 'AM';
+	}
+
+	echo form_dropdown('start_time', config_item('time_increments_hours_twelve'), $start_hour, 'id="start_time"');
+	echo form_dropdown('start_meridian', config_item('time_meridian'), date('A'), 'id="start_meridian"');
+	echo ' to ';
 	echo form_dropdown('end_time', config_item('time_increments_hours_twelve'), $end_hour, 'id="end_time"');
 	echo form_dropdown('end_meridian', config_item('time_meridian'), $end_meridian, 'id="end_meridian"'); 
 	?>
@@ -19,8 +35,8 @@
 /* Search Controls */
 #search_title 	{ float: left; }
 #search_title select { position: relative; top: -7px; left: 0px; }
-#start_time		{ margin: 0px 5px 0px 15px; }
-#start_meridian	{ margin: 0px 15px 0px 0px; }
+#start_time		{ margin: 0px 10px 0px 15px; }
+#start_meridian	{ margin: 0px 15px 0px 5px; }
 #end_time 		{ margin: 0px 10px 0px 15px; }
 #end_meridian 	{ margin: 0px 15px 0px 5px; }
 #search_button	{ float: left; margin: 2px 0px 0px 20px; }
@@ -39,17 +55,22 @@
 }
 
 div.hour_bar {
-	width: 220px;
+	width: 250px;
 	height: 100px;
 	font-size: 18px;
 	border-bottom: 1px solid #999;
 	clear: both;
 }
 div.hour_bar_time {
-	width: 50px;
+	width: 80px;
 	float: left;
+	text-align: center;
 	margin-right: 20px;
 	padding-top: 40px;
+}
+span.hour_time_meridian {
+	font-style: italic;
+	color: #999;
 }
 div.hour_bar_emotion {
 	width: 60px;
@@ -74,10 +95,16 @@ $(document).ready(function()
 	{
 		// Start Time
 		var start_time	= $('#start_time').val();
-		if ($('#start_meridian').val() == 'PM')
+		if (($('#start_meridian').val() == 'PM') && ($('#start_time').val() != 12))
 		{
 			start_time = parseInt(start_time) + 12;
 		}
+		else
+		{
+			start_time = parseInt(start_time);
+		}
+		
+		console.log(start_time);
 		
 		// End Time
 		var end_time 	= $('#end_time').val();
@@ -85,7 +112,7 @@ $(document).ready(function()
 		{
 			end_time = parseInt(end_time) + 12;
 		}
-			
+
 		// If One Time Value is Different
 		if ((start_time != $('#visualize_feelings').data('start')) || (end_time != $('#visualize_feelings').data('end')))	
 		{
@@ -99,16 +126,17 @@ $(document).ready(function()
 				type		: 'GET',
 				dataType	: 'json',
 			  	success		: function(result)
-			  	{							  	
-					console.log(result);
-
+			  	{
+			  		// Yay Feelings
 					if (result.status == 'success')
 					{
+						// Update Range
 						$('#visualize_feelings').data('start', start_time);
 						$('#visualize_feelings').data('end', end_time)
 					
 						var hour_array = new Array();
 					
+						// Loop Through Results
 						$.each(result.emotions, function(key, value)
 						{
 							var hour	= value.created_time.split(':');
@@ -126,8 +154,25 @@ $(document).ready(function()
 							}
 							else
 							{
+								// Add To Hour
 								hour_array.push(hour[0]);
-								$visualize_feelings.append('<div id="hour_bar_' + hour[0] + '" class="hour_bar"><div class="hour_bar_time">' + hour[0] + ':00</div>' + emotion + '</div>');
+								
+								if (hour[0] > 12)
+								{
+									var time_hour = parseInt(hour[0]) - 12;
+									var display_hour = time_hour + ' <span class="hour_time_meridian">PM</span>';
+								}
+								else if (hour[0] == 12)
+								{
+									var display_hour = hour[0] + ' <span class="hour_time_meridian">PM</span>';										
+								}
+								else
+								{
+									var display_hour = hour[0] + ' <span class="hour_time_meridian">AM</span>';	
+								}
+								
+								// Add Hour Row
+								$visualize_feelings.append('<div id="hour_bar_' + hour[0] + '" class="hour_bar"><div class="hour_bar_time">' + display_hour + '</div>' + emotion + '</div>');
 							}		
 						});
 					}
