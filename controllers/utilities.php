5d72f96d23ed9c5d521bb00ff9c483c64b212d8d
Brennan Novak <hi@brennannovak.com>
2012-09-20 15:03:28 -0700

    added more types_sub to config files and such

--- controllers/utilities.php
+++ controllers/utilities.php
@@ -193,7 +193,7 @@ class Utilities extends MY_Controller
 	{
 		$this->db->select('word_id, word, type_sub, speech');
 		$this->db->from('words');
-		$this->db->where('speech', NULL);
+		$this->db->where($this->uri->segment(4), NULL);
  		$query = $this->db->get();
  		$result = $query->result();
 
@@ -204,7 +204,7 @@ class Utilities extends MY_Controller
 		{
 			echo 'Updating: '.$word->word.'<br>';	
 		
-			$this->words_model->update_word($word->word_id, array('speech' => 'U'));
+			$this->words_model->update_word($word->word_id, array($this->uri->segment(4) => 'U'));
 		}
 	}
 	
